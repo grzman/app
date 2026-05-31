2587f8346875c84b8ee6f2e49af942b8d36adf45
Arttor-biuro7 <g.manuszewski@gmail.com>
2026-05-31 10:20:31 +0200

    Update from workspace

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Witaj MERITO!</h1><p>Autor: Bartosz Bryniarski</p>'
+    return '<h1>Witaj MERITO!</h1><p>Autor: Grzegorz Manuszewski</p>'
 
 if __name__ == '__main__':
     app.run(debug=True)
