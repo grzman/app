1f2f23e790afbd53651bdac912cf6080f07bd5c5
Arttor-biuro7 <g.manuszewski@gmail.com>
2026-05-31 10:07:55 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Witaj MERITO!</h1><p>Autor: Bartosz Bryniarski</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
